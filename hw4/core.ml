d69d97d0b5a4e5d7fc55c8f7dcc4c0560a2f7054
James Chan <jameschan312.cn@gmail.com>
2024-03-25 18:05:29 -0400

    Mar 25, 2024

--- hw4/core.ml
+++ hw4/core.ml
@@ -24,8 +24,8 @@ let updatestore (store: store) (n: int) (v: term) =
     | (n, v'::rest) -> v' :: (f (n-1,rest))
     | _ -> error dummyinfo "updatestore: bad index"
   in
-  
   f (n, store)
+
 let shiftstore i store = List.map (fun t -> termShift i t) store 
 
 
@@ -37,39 +37,62 @@ exception NoRuleApplies
 (* The small step semantics are shown on slides 8/36 and 9/36. *)
 (* HINT: To substitute v in t for all top variables (i.e., variables of index 0), *)
 (*               use function `termSubstTop v t`  defined in syntax.ml. *)
-let rec eval1 ctx store t =
+let rec eval1 (ctx: context) (store: store) (t: term) : term * store  =
   match t with
-  | TmVar(_, _, _) -> (t, store)
-  | TmAbs(_, _, _) -> (t, store)
-  | TmApp(fi, t1, t2) ->
-    if (isval ctx t1) then
-      begin 
-        match t1 with
-        | TmAbs(_, x, e) -> (e, store) (* TODO: beta reduction *)
-        | _ -> raise NoRuleApplies
-      end
-    else 
-      let t1', store' = eval1 ctx store t1 in
-      (TmApp(fi, t1', t2), store')
-  | TmLoc(_, _) -> (t, store)
-  | TmRef(fi, t1) -> 
-    if (isval ctx t1) then
-      let loc, store' = extendstore store t1 in
-      (TmLoc(fi, loc), store')
-    else
-      let t1', store' = eval1 ctx store t1 in
-      (TmRef(fi, t1'), store')
-  | TmDeref(fi, t1) -> 
+  | TmVar(_, _, _) -> 
+    (* Var *)
+    (t, store)  (* What should we do here? *)
+  | TmAbs(_, _, _) -> 
+    (* Abs *)
+    (t, store)
+  | TmLoc(_, _) ->
+    (* Loc *)
+    (t, store)
+  | TmApp(fi, t1, t2) when (isval ctx t1) ->
     begin 
       match t1 with
-      | TmLoc(_, l) -> (lookuploc store l, store)
-      | _ ->
-        let t1', store' = eval1 ctx store t1 in
-        (TmDeref(fi, t1'), store')
+      | TmAbs(_, _, e) when (isval ctx t2) ->
+        (* App *)
+        ((termSubstTop t2 e), store)   (* Beta reduction *)
+      | TmAbs(_, _, _) ->
+        (* RApp *)
+        let t2', store' = eval1 ctx store t2 in
+        (TmApp(fi, t1, t2'), store')
+      | _ -> raise NoRuleApplies
     end
-  | TmAssign(_, _, _) -> (t, store)
+  | TmApp(fi, t1, t2) ->
+    (* LApp *)
+    let t1', store' = eval1 ctx store t1 in
+    (TmApp(fi, t1', t2), store')
+  | TmRef(fi, t1) when (isval ctx t1) ->
+    (* Ref *)
+    let loc, store' = extendstore store t1 in
+    (TmLoc(fi, loc), store')
+  | TmRef(fi, t1) ->
+    (* RefSimp *) 
+    let t1', store' = eval1 ctx store t1 in
+    (TmRef(fi, t1'), store')
+  | TmDeref(_, TmLoc(_, l)) ->
+    (* Deref *)
+    ((lookuploc store l), store)
+  | TmDeref(fi, t1) ->
+    (* DerefSimp *)
+    let t1', store' = eval1 ctx store t1 in
+    (TmDeref(fi, t1'), store')
+  | TmAssign(fi, TmLoc(_, l), t2) when (isval ctx t2) ->
+    (* Assign *)
+    let store' = (updatestore store l t2) in
+    (t2, store')
+  | TmAssign(fi, t1, t2) when (isval ctx t1) ->
+    (* RAssign *)
+    let t2', store' = eval1 ctx store t2 in
+    (TmAssign(fi, t1, t2'), store')
+  | TmAssign(fi, t1, t2) ->
+    (* LAssign *)
+    let t1', store' = eval1 ctx store t1 in
+    (TmAssign(fi, t1', t2), store')
 
 let rec eval ctx store t =
   try let t', store' = eval1 ctx store t
       in eval ctx store' t'
-  with NoRuleApplies -> raise NoRuleApplies
+  with NoRuleApplies -> t, store
